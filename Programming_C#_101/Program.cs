e5c1e0ebc27f6bd72998caa8b289c54853b346f2
Tri Le <wukong2906676@gmail.com>
2024-06-30 07:19:38 +0700

    [1] C#_101

---
+++ Programming_C#_101/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Part 1 Print a string to the console
+        Console.WriteLine("Hello, World!");
+
+        //Part 2 Get user input and print it to the console        
+        Console.WriteLine("What your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + ":)");
+
+        //Part 3 Get two inputs from the user and print them on separate lines
+        Console.WriteLine("How old are you?");
+        string old = Console.ReadLine();
+        Console.WriteLine("Hello " + name + "\n" + "Old: " + old);
+    }
+}
